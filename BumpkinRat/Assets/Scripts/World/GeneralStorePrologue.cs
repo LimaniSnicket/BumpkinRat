e5c46970bd0bb3a3e2b98ed2dfeba5c60cd6152e
Vera Limani <veralimani@microsoft.com>
2020-09-17 21:23:40 -0700

    Added PrologueHUD and Time Span tracking

--- BumpkinRat/Assets/Scripts/World/GeneralStorePrologue.cs
+++ BumpkinRat/Assets/Scripts/World/GeneralStorePrologue.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,13 +7,29 @@ public class GeneralStorePrologue : MonoBehaviour
 {
     RealTimeCounter prologueCounter;
 
+    TimeSpan startTime;
+    TimeSpan addOneSecond;
+
     private void Start()
     {
         prologueCounter = new RealTimeCounter(0.5f, TimeUnitToTrack.Minute);
+        startTime = new TimeSpan(12, 14, 27);
+        addOneSecond = new TimeSpan(0, 0, 1);
+        StartCoroutine(AddToTimeSpan());
     }
 
     private void Update()
     {
         prologueCounter.DecrementTimerOverTime();
+        PrologueHUD.SetTimerDisplayMessage(startTime.ToString());
+    }
+
+    IEnumerator AddToTimeSpan()
+    {
+        while (gameObject.activeSelf)
+        {
+            yield return new WaitForSeconds(1);
+            startTime = startTime.Add(addOneSecond);
+        }
     }
 }
